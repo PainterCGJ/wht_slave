011fdee6833748f76c7fbf8862ef77b6e5528f92
rainlong <ylongwangi@qq.com>
2025-07-29 13:16:07 +0800

    feat: add protocol and FreeRTOScpp wrappter

--- User/App/slave_app.cpp
+++ User/App/slave_app.cpp
@@ -8,14 +8,10 @@ extern void UWB_Task_Init(void);
 const char *TAG = "slave_app";
 
 extern "C" int main_app(void) {
-    // elog_i(TAG, "slave_app");
     UWB_Task_Init();
 
-    uint8_t data[] = {0x00, 0x01, 0x02, 0x03};
     for (;;) {
-        // Send UWB data with optional delay
-        int result = UWB_SendData(data, sizeof(data), 0);
-        // elog_i(TAG, "slave_app");
+
         HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
         osDelay(100);
     }
